8d4943a551254b2b9806e251908ee89d097a0eb5
Jeremy White <jeremy.white@idean.com>
2014-08-15 14:33:39 -0700

    Documented MediaInfo

--- Helpers/MediaInfo.h
+++ Helpers/MediaInfo.h
@@ -22,18 +22,49 @@
 #import "ImageInfo.h"
 
 
+/*!
+ * Normalized reference object for information about a media file to be sent to a device through the MediaPlayer capability. "Media file", in this context, refers to an audio or video resource.
+ */
 @interface MediaInfo : NSObject
 
+/*! URL source of the media file */
 @property (nonatomic, strong) NSURL *url;
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, strong) NSString *description;
+
+/*! Mime-type of the media file */
 @property (nonatomic, strong) NSString *mimeType;
+
+/*! Title of the media file (optional) */
+@property (nonatomic, strong) NSString *title;
+
+/*! Short description of the media file (optional) */
+@property (nonatomic, strong) NSString *description;
+
+/*! Duration of the media file */
 @property (nonatomic) NSTimeInterval duration;
+
+/*! Collection of ImageInfo objects to send, as necessary, to the device when launching media through the MediaPlayer capability. */
 @property (nonatomic, strong) NSArray *images;
 
+/*
+ * Creates an instance of MediaInfo with given property values.
+ *
+ * @param url URL source of the media file
+ * @param mimeType Mime-type of the media file
+ */
 - (instancetype) initWithURL:(NSURL *)url mimeType:(NSString *)mimeType;
 
+/*!
+ * Adds an ImageInfo object to the array of images.
+ *
+ * @param image ImageInfo object to be added
+ */
 - (void) addImage:(ImageInfo *)image;
+
+/*!
+ * Adds an array of ImageInfo objects to the array of images.
+ *
+ * @param images Array of ImageInfo objects to be added
+ */
 - (void) addImages:(NSArray *)images;
 
 @end
