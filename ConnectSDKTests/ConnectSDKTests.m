a7eae72a30d0f7dd8532f3085eba3a3357c91902
Eugene Nikolskyi <eugene.nikolskyi@lge.com>
2014-11-11 16:22:36 -0800

    Create first test of default platforms

--- ConnectSDKTests/ConnectSDKTests.m
+++ ConnectSDKTests/ConnectSDKTests.m
@@ -9,32 +9,19 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "ConnectSDKDefaultPlatforms.h"
+
+/// Generic tests for the ConnectSDK.
 @interface ConnectSDKTests : XCTestCase
 
 @end
 
 @implementation ConnectSDKTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+/// Tests that the default platforms list is defined and not empty.
+- (void)testThereShouldBeDefaultPlatforms {
+    NSDictionary *platforms = kConnectSDKDefaultPlatforms;
+    XCTAssertGreaterThan(platforms.count, 0, @"The default platforms list must not be empty");
 }
 
 @end
