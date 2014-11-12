f60c469eeaf7f173be34a524166685efdcf3f232
Eugene Nikolskyi <eugene.nikolskyi@lge.com>
2014-11-11 17:10:31 -0800

    Tests for default service and discovery provider classes availability

--- ConnectSDKTests/ConnectSDKTests.m
+++ ConnectSDKTests/ConnectSDKTests.m
@@ -24,4 +24,29 @@
     XCTAssertGreaterThan(platforms.count, 0, @"The default platforms list must not be empty");
 }
 
+/// Tests that the default service classes are available in runtime.
+/// NB: the success of this test relies on the test target's setup.
+- (void)testServiceClassesAreAvailable {
+    NSDictionary *platforms = kConnectSDKDefaultPlatforms;
+    [platforms enumerateKeysAndObjectsWithOptions:NSEnumerationConcurrent
+                                       usingBlock:^(NSString *key, id obj, BOOL *stop) {
+                                           XCTAssertNotNil(NSClassFromString(key),
+                                                           @"Service class %@ must be available at runtime", key);
+                                       }];
+}
+
+/// Tests that the default discovery provider classes are available at runtime.
+/// NB: the success of this test relies on the test target's setup.
+- (void)testDiscoveryProviderClassesAreAvailable {
+    NSDictionary *platforms = kConnectSDKDefaultPlatforms;
+    // we get the unique providers only because one can be used for different
+    // services
+    NSSet *uniqueDiscoveryProviders = [NSSet setWithArray:platforms.allValues];
+    [uniqueDiscoveryProviders enumerateObjectsWithOptions:NSEnumerationConcurrent
+                                               usingBlock:^(NSString *obj, BOOL *stop) {
+                                                   XCTAssertNotNil(NSClassFromString(obj),
+                                                                   @"Discovery provider class %@ must be available at runtime", obj);
+                                               }];
+}
+
 @end
