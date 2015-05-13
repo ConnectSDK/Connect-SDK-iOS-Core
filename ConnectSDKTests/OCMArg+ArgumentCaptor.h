1b113ee4b83bf7f4328272bf1c609bf8893cc178
Eugene Nikolskyi <eugene.nikolskyi@lge.com>
2015-05-13 12:26:17 -0700

    Create OCMArg+ArgumentCaptor category

---
+++ ConnectSDKTests/OCMArg+ArgumentCaptor.h
@@ -0,0 +1,30 @@
+//
+//  OCMArg+ArgumentCaptor.h
+//  ConnectSDK
+//
+//  Created by Eugene Nikolskyi on 5/13/15.
+//  Copyright (c) 2015 LG Electronics. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import <OCMock/OCMArg.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface OCMArg (ArgumentCaptor)
+
+/// Convenience method to capture an argument for later use/verification.
++ (id)captureTo:(out __strong __nullable id *__nonnull)objectPointer;
+
+@end
+NS_ASSUME_NONNULL_END
