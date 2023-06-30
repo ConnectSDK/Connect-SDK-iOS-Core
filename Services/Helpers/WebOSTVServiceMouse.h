fdcb12fbcd3fa0804e709e3e13970f95cd62a694
Nikiteizzz <37298569+Nikiteizzz@users.noreply.github.com>
2023-06-30 15:18:09 +0300

    fix: updated webos buttons enum

--- Services/Helpers/WebOSTVServiceMouse.h
+++ Services/Helpers/WebOSTVServiceMouse.h
@@ -22,13 +22,38 @@
 #import "Capability.h"
 
 typedef enum {
-    WebOSTVMouseButtonHome = 1000,
-    WebOSTVMouseButtonBack = 1001,
-    WebOSTVMouseButtonUp = 1002,
-    WebOSTVMouseButtonDown = 1003,
-    WebOSTVMouseButtonLeft = 1004,
-    WebOSTVMouseButtonRight = 1005
+WebOSTVMouseButtonPower = 1001,
+WebOSTVMouseButtonUp = 1002,
+WebOSTVMouseButtonDown = 1003,
+WebOSTVMouseButtonRight = 1004,
+WebOSTVMouseButtonLeft = 1005,
+WebOSTVMouseButtonMenu = 1006,
+WebOSTVMouseButtonHome = 1007,
+WebOSTVMouseButtonBack = 1008,
+WebOSTVMouseButtonExit = 1009,
+WebOSTVMouseButtonOk = 1010,
+WebOSTVMouseButtonVolumeUp = 1011,
+WebOSTVMouseButtonVolumeDown = 1012,
+WebOSTVMouseButtonChannelUp = 1013,
+WebOSTVMouseButtonChannelDown = 1014,
+WebOSTVMouseButtonSource = 1015,
+WebOSTVMouseButtonZero = 1016,
+WebOSTVMouseButtonOne = 1017,
+WebOSTVMouseButtonTwo = 1018,
+WebOSTVMouseButtonThree = 1019,
+WebOSTVMouseButtonFour = 1020,
+WebOSTVMouseButtonFive = 1021,
+WebOSTVMouseButtonSix = 1022,
+WebOSTVMouseButtonSeven = 1023,
+WebOSTVMouseButtonEight = 1024,
+WebOSTVMouseButtonNine = 1025,
+WebOSTVMouseButtonMute = 1026,
+WebOSTVMouseButtonPlay = 1027,
+WebOSTVMouseButtonPause = 1028,
+WebOSTVMouseButtonNext = 1029,
+WebOSTVMouseButtonPrev = 1030
 } WebOSTVMouseButton;
+
 
 @interface WebOSTVServiceMouse : NSObject
 
